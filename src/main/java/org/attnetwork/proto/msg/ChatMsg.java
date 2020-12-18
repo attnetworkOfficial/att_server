c09bc45953da91a00f87fc1c73d4e63cf4feb2e3
attnetworkOfficial <github1@attnetwork.org>
2020-12-18 15:31:35 +0800

    chat protocol update

--- src/main/java/org/attnetwork/proto/msg/ChatMsg.java
+++ src/main/java/org/attnetwork/proto/msg/ChatMsg.java
@@ -1,11 +1,24 @@
 package org.attnetwork.proto.msg;
 
 import org.attnetwork.proto.sl.AbstractSeqLanObject;
+import org.attnetwork.utils.BitmapFlags;
 
 public final class ChatMsg extends AbstractSeqLanObject {
   public Integer id;
-  public Type type;
+  public BitmapFlags<Flags> flags;
   public String title;
+  public ChatPhoto photo;
+  public Integer membersCount;
+
+  public enum Flags {
+    // user flags
+    IS_CREATOR,
+    IS_KICKED,
+    IS_LEFT,
+    IS_DEACTIVATED,
+  }
+
+  public Type type;
   public String username;
   public String first_name;
   public String last_name;
